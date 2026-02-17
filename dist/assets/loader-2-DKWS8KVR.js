30ce81f288f6d02025279b4df41c96e91f2ecae3
Franco R <franco.l.repetti@gmail.com>
2026-02-17 18:24:20 -0300

    Blank Page Fix, added index.html y main.tsx

--- dist/assets/loader-2-DKWS8KVR.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./createLucideIcon-B_1fxKTs.js";const c=e("CheckCircle",[["path",{d:"M22 11.08V12a10 10 0 1 1-5.93-9.14",key:"g774vq"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);const o=e("Loader2",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{c as C,o as L};
